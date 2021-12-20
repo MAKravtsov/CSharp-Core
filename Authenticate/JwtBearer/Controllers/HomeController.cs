fc75712d8a5794c60de8d38f202158eca5352b13
mkravtsov <ma_kravtsov@mail.ru>
2021-12-20 19:54:43 +0300

    Jwt Bearer

---
+++ Authenticate/JwtBearer/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtBearer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var userName = "Maksim";
+            var userEmail = "Maksim@mail.ru";
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userName),
+                new Claim(JwtRegisteredClaimNames.Sub, userEmail)
+            };
+
+            var now = System.DateTime.Now;
+
+            byte[] secretBytes = Encoding.UTF8.GetBytes(Constants.Secret_key);
+
+            var key = new SymmetricSecurityKey(secretBytes);
+
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(Constants.Issuer, Constants.Audience, claims,
+                notBefore: now, expires: now.AddHours(1), signingCredentials);
+
+            var value = new JwtSecurityTokenHandler().WriteToken(token);
+
+            ViewBag.Token = value;
+
+            return View();
+        }
+    }
+}
---
+++ Authenticate/JwtBearer/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JwtBearer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string jwtName = "OAuth";
+            services.AddAuthentication(jwtName)
+                .AddJwtBearer(jwtName, config =>
+                {
+                    byte[] secretBytes = Encoding.UTF8.GetBytes(Constants.Secret_key);
+
+                    var key = new SymmetricSecurityKey(secretBytes);
+
+                    config.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = Constants.Issuer,
+                        ValidAudience = Constants.Audience,
+                        IssuerSigningKey = key
+                    };
+                });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
